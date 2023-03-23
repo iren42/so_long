1524b6ee6eb2294449112b7d79810ae80914cf33
Isabelle REN <iren@e2r6p9.clusters.42paris.fr>
2023-03-23 22:20:53 +0100

    removed duplicate function

--- src/ft_refresh_img.c
+++ src/ft_refresh_img.c
@@ -12,35 +12,6 @@
 
 #include "so_long.h"
 
-static int	render_player(t_img *img, t_player p, t_img t)
-{
-	int		n;
-	int		m;
-	int		cl;
-	float	tx;
-	float	ty;
-
-	n = p.y * TILE_SIZE;
-	if (img != 0)
-	{
-		while (n < (p.y + 1) * TILE_SIZE)
-		{
-			m = p.x * TILE_SIZE;
-			while (m < (p.x + 1) * TILE_SIZE)
-			{
-				tx = ((float)m) / TILE_SIZE * t.hei;
-				ty = ((float)n) / TILE_SIZE * t.wid;
-				cl = ft_get_pix_color(&t, ((int)tx) % t.hei, ((int)ty) % t.wid);
-				if (ft_get_pix_color(img, m, n) != cl)
-					ft_img_pix_put(img, m, n, cl);
-				m++;
-			}
-			n++;
-		}
-	}
-	return (0);
-}
-
 static int	render_tile(t_img *img, int j, int i, t_img t)
 {
 	int		n;
@@ -103,7 +74,7 @@ int	ft_refresh_img(t_data *d)
 	if (d)
 	{
 		render_map(&d->img);
-		render_player(&d->img, d->img.player, d->tex[player]);
+		render_tile(&d->img, d->img.player.x, d->img.player.y, d->tex[player]);
 		if (d->img.mlx_img != NULL && d->win_ptr != NULL && d->mlx_ptr != NULL)
 			mlx_put_image_to_window(d->mlx_ptr, d->win_ptr,
 				d->img.mlx_img, 0, 0);
